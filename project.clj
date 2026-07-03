6ea4616c6dffc6e33bfb71468f797a2f273e07eb
Markus Penttilä <markus.penttila@cloudpermit.com>
2026-07-03 18:21:03 +0300

    Release as fi.lupapiste/lein-bom "0.3.0"

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject lein-bom "0.2.0-SNAPSHOT"
+(defproject fi.lupapiste/lein-bom "0.3.0"
   :description "A leiningen plugin that provides support for importing maven BOMs"
-  :url "https://github.com/tsachev/lein-bom"
+  :url "https://github.com/lupapiste/lein-bom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.8.1"
   :eval-in :leiningen
   :plugins [[lein-cloverage "1.0.10"]]
-  :deploy-repositories [["clojars" {:username :env/CLOJARS_USERNAME :password :env/CLOJARS_PASSWORD}]])
+  :deploy-repositories [["clojars" {:username      :env/clojars_username
+                                    :password      :env/clojars_token
+                                    :sign-releases false}]])
